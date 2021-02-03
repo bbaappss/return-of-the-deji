be0bbd390ac7fb6a23177522e15bb4df4d3d2431
Brandon Scott <email@brandonandrewscott.com>
2021-02-03 10:33:05 -0800

    Lots of menu work

--- dist/js/site.js
+++ dist/js/site.js
@@ -608,6 +608,10 @@ window.setTimeout(function(){$(".loading").addClass("not-loading");}, 3000);
 
 var tl = gsap.timeline({repeat: 0});
 
+tl.set(".loading img", {
+  visibility: "visible"
+})
+
 tl.from(".loading img", {
   opacity: 0,
   duration: .5,
@@ -643,24 +647,27 @@ tl.from(".phone-breakout img", {
   }
 )
 
-tl.from(".cta", {
-    opacity: 0,
-    y: 10,
-    duration: .5
-  }
-)
-
 tl.from("#navigation .burger-and-fries", {
   y: -20,
   opacity: 0,
   duration: .5
 }, "loadTheRest")
 
+tl.from("#navigation .menu-on-large .main-nav a", {
+  x: 50,
+  y: -40,
+  opacity: 0,
+  duration: .2,
+  stagger: {
+    amount: .5
+  },
+}, "loadTheRest")
+
 tl.from(".homepage-hero .copy *", {
   opacity: 0,
   y: 10,
-  duration: .5,
-  stagger: .6
+  duration: .6,
+  stagger: .2
 }, "loadTheRest")
 
 tl.from(".slider-arrow.left", {
@@ -679,12 +686,38 @@ tl.from(".slider-arrow.right", {
 }, "sliders"
 )
 
+tl.from(".cta", {
+  opacity: 0,
+  y: 10,
+  duration: .5
+}
+)
+
 tl.from(".down-arrow", {
   opacity: 0,
   y: -10,
-  duration: .5,
+  duration: .5
 }
 )
+
+
+const downarrowTL = gsap.timeline( { 
+   
+  scrollTrigger: {
+    trigger: ".down-arrow",
+    start: "-=300 55%",
+    end: "100 70%",
+    scrub: true,
+    toggleActions: "play reverse play reverse",
+  }
+  
+});
+
+downarrowTL
+  .to('.down-arrow', { opacity: 0, duration: 1 }, 0.5)
+;
+
+
 $(window).on("load", function () {
 
   // set up hamburger stuff

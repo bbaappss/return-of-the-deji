9c73023cf6bad8beb792fe33c8bedd59642de421
Brandon Scott <email@brandonandrewscott.com>
2021-02-08 16:39:11 -0800

    Kill all TripleLift related stuff

--- dist/js/site.js
+++ dist/js/site.js
@@ -604,63 +604,6 @@ var Lantern = (function() {
 
 })();
 
-const whyTLGradientTL = gsap.timeline( {    
-  scrollTrigger: {
-    trigger: ".why-triplelift",
-    start: "40% bottom",
-    end: "60% bottom",
-    scrub: true,
-    toggleActions: "play reverse play reverse",
-  }
-});
-
-whyTLGradientTL
-  .to(
-    '.gradient-transition.gradient2', 
-    { 
-      opacity: 1, 
-      duration: 1 
-    }, "why-triplelift"
-);
-
-const exploreGalleryGradientTL = gsap.timeline( {    
-  scrollTrigger: {
-    trigger: ".explore-our-gallery-container",
-    start: "-70% center",
-    end: "60% center",
-    scrub: true,
-    toggleActions: "play reverse play reverse",
-  }
-});
-
-
-exploreGalleryGradientTL
-  .to(
-    '.gradient-transition.white', 
-    { 
-      opacity: 1, 
-      duration: 1 
-    }, "explore"
-);
-
-const weWorkWithGradientTL = gsap.timeline( {    
-  scrollTrigger: {
-    trigger: ".we-work-with",
-    start: "top center",
-    end: "60% center",
-    scrub: true,
-    toggleActions: "play reverse play reverse",
-  }
-});
-
-weWorkWithGradientTL
-  .to(
-    '.gradient-transition.white', 
-    { 
-      opacity: 0, 
-      duration: 0
-    }, "we-work-with"
-);
 
 window.setTimeout(function(){$(".loading").addClass("not-loading");}, 3000);
 

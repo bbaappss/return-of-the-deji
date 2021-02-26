c4cf8f34d3f036d7505e5a37f7b68838a53e93f2
Brandon Scott <email@brandonandrewscott.com>
2021-02-26 11:27:26 -0800

    Small viewport for nebula article

--- dist/js/site.js
+++ dist/js/site.js
@@ -604,32 +604,6 @@ var Lantern = (function() {
 
 })();
 
-window.setTimeout(function(){$(".loading").addClass("not-loading");}, 3000);
-
-var tl = gsap.timeline({repeat: 0});
-
-tl.set(".loading img", {
-  visibility: "visible"
-})
-
-tl.from(".loading img", {
-  opacity: 0,
-  duration: .5,
-  y: 50,
-  delay: .5
-})
-
-tl.to(".loading img", {
-  opacity: 0,
-  duration: .5,
-  y: -50,
-  delay: 1
-})
-
-tl.to(".loading", {
-  opacity: 0,
-  duration: .5,
-})
 
 $(window).on("load", function () {
 

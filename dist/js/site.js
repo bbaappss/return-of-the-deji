11e512979604039ca3bf200ce827b3300641d7a9
Brandon Scott <email@brandonandrewscott.com>
2021-03-02 15:52:17 -0800

    Fix up the markers

--- dist/js/site.js
+++ dist/js/site.js
@@ -667,20 +667,54 @@ document.addEventListener('scroll', debounce(storeScroll), {
 
 // Update scroll position for first time
 storeScroll();
-const sectionTL = gsap.timeline( {    
-    scrollTrigger: {
-      trigger: ".section2",
-      scrub: true,
-      // markers: true,
-      toggleActions: "play reverse play reverse",
-    }
-  });
+gsap.registerPlugin(ScrollTrigger);
+
+/*** Different ScrollTrigger setups for various screen sizes (media queries) ***/
+ScrollTrigger.matchMedia({
+
+  "(min-width: 1024px)": function() {
+    // setup animations and ScrollTriggers for screens over 800px wide (desktop) here...
+    // ScrollTriggers will be reverted/killed when the media query doesn't match anymore.
+    let tl = gsap.timeline({
+          scrollTrigger: {
+          trigger: ".section2",
+          scrub: true,
+        }
+      });
+    tl.to(".section-transition.bgimage", {backgroundPosition: "0 60%"})
+  },
   
-sectionTL
-    .to(
-      '.section-transition.bgimage', 
-    { 
-      // backgroundPosition: "0 -100px",
-      duration: 1 
-    }
-);
+  "(min-width: 768px)": function() {
+    // setup animations and ScrollTriggers for screens over 800px wide (desktop) here...
+    // ScrollTriggers will be reverted/killed when the media query doesn't match anymore.
+    let tl = gsap.timeline({
+          scrollTrigger: {
+          trigger: ".section2",
+          scrub: true,
+        }
+      });
+    tl.to(".section-transition.bgimage", {backgroundPosition: "0 100%"})
+  }, 
+  
+  // mobile
+  "(max-width: 767px)": function() {
+    // Any ScrollTriggers created inside these functions are segregated and get
+    // reverted/killed when the media query doesn't match anymore. 
+    let tl = gsap.timeline({ 
+        scrollTrigger:{
+          trigger: ".section2",
+          scrub:true,
+        }
+      });
+      tl.to(".section-transition.bgimage", {
+        backgroundPosition: "0 -100px",
+      duration: 1})
+    }, 
+  
+  // all 
+  "all": function() {
+    // ScrollTriggers created here aren't associated with a particular media query,
+    // so they persist.
+  }
+  
+});
